4f6cd43c89c619f912b4be4e24dce3a7d037aebc
RICARDO ESCOBAR CESEÑA <95449905+XPFLASH@users.noreply.github.com>
2024-09-01 13:50:48 -0700

    commit

---
+++ Algoritmo_Ruta/ruta.py
@@ -0,0 +1,9 @@
+""" 
+    Materia: Desarrollo de Backend II
+    Algoritmo de Ruta
+
+    En base a una matriz, el algoritmo debera buscar la ruta con menos y mas costo para llegar 
+    del punto I al punto F, donde se debera imprimir el costo de las dos rutas.
+"""
+
+# El codigo debera ser en Python, no se permite como entrega un algoritmo de Dijkstra
